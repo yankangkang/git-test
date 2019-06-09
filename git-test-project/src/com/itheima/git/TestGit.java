b2ba9132de628aa2d889fb912f6a32a66c300ce7
yan <1401691000@qq.com>
2019-06-09 14:14:43 +0800

    修改后把本地工程添加到git

--- git-test-project/src/com/itheima/git/TestGit.java
+++ git-test-project/src/com/itheima/git/TestGit.java
@@ -7,5 +7,13 @@ public class TestGit {
         System.out.println("nihao  git");
         System.out.println("nihao  git");
         System.out.println("nihao  git");
+
+
+        System.out.println("22222222222");
+        System.out.println("22222222222");
+        System.out.println("22222222222");
+        System.out.println("22222222222");
     }
+
+
 }
