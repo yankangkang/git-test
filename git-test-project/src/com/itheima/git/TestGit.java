957f806a77121f054cc8b2da88171980b1b80d36
yan <1401691000@qq.com>
2019-06-09 14:13:21 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/TestGit.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("nihao  git");
+        System.out.println("nihao  git");
+        System.out.println("nihao  git");
+        System.out.println("nihao  git");
+        System.out.println("nihao  git");
+    }
+}
